0edd236c6b0db6932d7ac34a8d98f8916167a483
David Connolly <daveconnolly90@gmail.com>
2017-10-16 17:02:10 +0100

    Update project

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-front-end-stack
+# Front End Stack
+A small front end tool to assist with day to day task automation.
+
+## Features
+- NPM dependency manager
+- Gulp task automation
+- SCSS compilation
+- JS compilation
+- Browserify support
+- JSHint support
+- Bootstrap SASS v3.3.7
+- Materialize SASS v0.100.2
+
+## Setup Instructions
+- Install NodeJS: https://nodejs.org/en/download/
+- Open a terminal
+- Install GulpJS CLI by running this command: npm install gulp-cli -g
+- Navigate to your project directory in your terminal.
+- Install dependencies: npm install
+
+## Gulp Instructions
+- "gulp css" compiles scss files from source to build, postfixes file to .min and applies compression once. 
+- "gulp jshint" checks source/main.js and any required dependencies for errors based on the .jshintrc config file in root. 
+- "gulp js" runs "gulp jshint", applies browserify to main.js, forces ES2015 standards, bundles and pipes to build/js, postfixes file to .min and applies compression once.
+- "gulp clean" removes all build files.
+- "gulp build" runs "gulp css" then "gulp js".
+- "gulp watch" watches files in the source/scss and source/js directories then runs "gulp css" or "gulp js" based on the directory. It will then continue to watch for another change.
+
+# Notes
+- Bootstrap is used for its grid features and Materialize for the more aesthetic components.
+- I have included two examples.
+- index.html contains an out of the box materialize form with more appropriate checkboxes.
+- bootstrap-container.html is an example of how the fixed width container should be implemented so that containers hold their sizes at different breakpoints.
+  - We have made a decision to remove the offset on the left container at 992px. A simple change that looks much nicer on tablets before hitting desktop.
+- Follow the setup instructions and run a "gulp build" before looking at the examples.
---
+++ gulpfile.js
@@ -0,0 +1,112 @@
+/* 
+ * GulpJS config file. Helps us automate tasks that usually require a lot of work.
+ * Tasks we're going to automate include: SCSS compilation, css prefixing, css compression, JS concatenation, JS compression, file cleaning and so on.
+ */
+
+/* Setup some variables for use here */
+var scss_paths = {
+  input: "./source/scss/styles.scss",
+  maps: "./source/scss/maps",
+  output: "./build/css",
+  watch: "source/scss/**/*.scss"
+};
+
+var js_paths = {
+  input: "./source/js/**/*.js",
+  main: "./source/js/main.js",
+  output: "./build/js"
+};
+
+var scss_options = {
+  errLogToConsole: true,
+  outputStyle: 'compressed'
+};
+
+// Auto prefixer options are based on bootstrap standards. Until we drop it, lets stay inline with 3.3.7 support.
+var autoprefixer_options = {
+  browsers: [
+    "Android 2.3",
+    "Android >= 4",
+    "Chrome >= 20",
+    "Firefox >= 24",
+    "Explorer >= 8",
+    "iOS >= 6",
+    "Opera >= 12",
+    "Safari >= 6"
+  ]
+};
+
+/* Load GulpJS plugins */
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    clean = require('gulp-clean'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify');
+    sourcemaps = require('gulp-sourcemaps'),
+    browserify = require('browserify'),
+    babelify = require('babelify'),
+    buffer = require('vinyl-buffer')
+    source = require('vinyl-source-stream');
+
+/* Piping - lets us funnel plugin functionality into a certain order contained within a task */
+// CSS Task
+gulp.task('css', function(){
+  return gulp.src(scss_paths.input)
+    .pipe(sass())
+    .pipe(sass(scss_options).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(autoprefixer(autoprefixer_options))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(scss_paths.output))
+    .pipe(notify({ message: 'CSS task complete!' }));
+});
+
+// JSHint task
+gulp.task('jshint', function(){
+  return gulp.src(js_paths.main)
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('default'))
+});
+
+// JS Task
+gulp.task('js', ['jshint'], function () {
+  var b = browserify({
+    entries: js_paths.main,
+    debug: true,
+    transform: [babelify.configure({
+      presets: ['es2015']
+    })]
+  });
+
+  return b.bundle()
+    .pipe(source(js_paths.main))
+    .pipe(buffer())
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('build/js'))
+    .pipe(notify({ message: 'JS task complete!' }));
+});
+
+// Clean Task
+gulp.task('clean', function(){
+  return gulp.src(['build/css', 'build/js'], {read: false})
+  .pipe(clean())
+  .pipe(notify({ message: 'Clean up complete!' }));
+});
+
+// Build Task - cleans directories, then runs all independant tasks in order.
+gulp.task('build', ['css', 'js']);
+
+// Watch Task - execute during development to update changes on the fly.
+gulp.task('watch', function(){
+  
+  gulp.watch('source/scss/**/*.scss', ['css']); // Watch SCSS files
+  
+  gulp.watch('source/js/**/*.js', ['js']); // Watch JS files
+
+});
